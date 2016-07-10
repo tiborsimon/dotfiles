31f321ab0125875d68d76baaa9f8e255506ca447
Tibor Simon <tibor@tiborsimon.io>
2016-07-11 00:18:28 +0200

    Emacs config started.

---
+++ emacs/config/eyecandy/my-eyecandy.el
@@ -0,0 +1,54 @@
+
+; (use-package zenburn-theme
+;    :ensure zenburn-theme
+;    :config
+;    (progn
+;      (unless noninteractive
+;      (load-theme 'zenburn t))))
+
+(use-package aurora-theme
+   :ensure aurora-theme
+   :config
+   (progn
+     (unless noninteractive
+     (load-theme 'aurora t))))
+
+; (global-hl-line-mode t)
+; (set-face-background 'hl-line "#3e4446")
+
+; ;; Show parentheses
+; (show-paren-mode 1)
+
+; ;; highlight entire expression when matching paren is not visible;
+; ;; otherwise just highlight matching paren
+; (setq show-paren-style 'mixed)
+
+; (setq whitespace-style '(trailing))
+; (global-whitespace-mode 1)
+
+; (use-package smart-mode-line
+;   :ensure smart-mode-line
+;   :config
+;   (progn
+;     (setq sml/theme 'dark)
+;     (setq sml/mode-width 30)
+;     (sml/setup)
+;     ))
+
+; (use-package rainbow-mode
+;   :ensure rainbow-mode)
+
+; (use-package rainbow-delimiters
+;   :ensure rainbow-delimiters
+;   :init
+;   (progn
+;     (rainbow-delimiters-mode-enable)
+;     ))
+
+; (defun my-coding-mode-eyecandy ()
+;   "Eyecandy specific to programming text editing modes."
+;   (rainbow-delimiters-mode-enable))
+
+; (require 'my-modeline)
+
+(provide 'my-eyecandy)
--- emacs/init.el
+++ emacs/init.el
@@ -1,28 +1,122 @@
-(require 'cl)
+(add-to-list 'load-path (concat user-emacs-directory "config"))
+(add-to-list 'load-path (concat user-emacs-directory "config/eyecandy"))
+(add-to-list 'load-path (concat user-emacs-directory "config/languages"))
+(add-to-list 'load-path (concat user-emacs-directory "elisp"))
+
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
 (require 'package)
-
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (package-initialize)
+(setq package-enable-at-startup nil)
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(require 'use-package)
 
-(defvar my-packages '(evil evil-leader company))
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(require 'my-core)
+(require 'my-evil)
+(require 'my-eyecandy)
 
-(require 'evil)
-; (require 'init-evil)
-(evil-mode 1)
+; ;; Essential settings.
+; (setq inhibit-splash-screen t
+;       inhibit-startup-message t
+;       inhibit-startup-echo-area-message t)
+; (menu-bar-mode -1)
+; (tool-bar-mode -1)
+; (when (boundp 'scroll-bar-mode)
+;   (scroll-bar-mode -1))
+; (show-paren-mode 1)
+; (visual-line-mode 1)
+; (setq require-final-newline t)
 
-(global-evil-leader-mode)
-(evil-leader/set-leader "<SPC>")
-(setq evil-leader/in-all-states 1)
-(evil-leader/set-key
-    "e" 'find-file
-    "b" 'switch-to-buffer
-    "q" 'kill-buffer)
+; ;; Smooth scrolling
+; (setq scroll-margin 5
+;       scroll-conservatively 9999
+;       scroll-step 1)
+
+
+
+; ;; EVIL MODE
+; (require 'evil)
+; ; (require 'init-evil)
+; (evil-mode 1)
+; (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+; (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+; (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+; (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+; (define-key evil-motion-state-map ";" 'evil-ex)
+
+
+; (use-package evil-leader
+;       :commands (evil-leader-mode)
+;       :ensure evil-leader
+;       :demand evil-leader
+;       :init
+;       (global-evil-leader-mode)
+;       :config
+;       (progn
+; 	(evil-leader/set-leader "<SPC>")
+; 	(setq evil-leader/in-all-states 1)
+; 	(evil-leader/set-key "e" 'find-file)
+; 	(evil-leader/set-key "b" 'switch-to-buffer)
+; 	(evil-leader/set-key "w" 'save-buffer)
+; 	(evil-leader/set-key "q" 'kill-buffer-and-window)
+; 	(evil-leader/set-key "h" 'dired-jump)
+; 	(evil-leader/set-key "v" 'split-window-right)
+; 	(evil-leader/set-key "e" 'pp-eval-last-sexp)
+; 	(evil-leader/set-key "," 'other-window)
+; 	(evil-leader/set-key "b" 'ibuffer)
+; 	(evil-leader/set-key "x" 'helm-M-x)
+;         )
+;       )
+
+
+; (use-package evil-leader
+;       :commands (evil-leader-mode)
+;       :ensure key-chord
+;       :demand key-chord
+;       :config
+;       (progn
+; 	(key-chord-mode 1)
+; 	(key-chord-define evil-insert-state-map  "jk" 'evil-normal-state)
+; 	(key-chord-define evil-visual-state-map  "jk" 'evil-normal-state)
+;         )
+;       )
+
+
+; ;; POWERLINE
+; (require 'powerline)
+; ; (powerline-evil-vim-color-theme)
+; (display-time-mode t)
+
+
+; ; (load-theme 'aurora t)
+
+; ;; Elisp navigation speedup
+; (progn
+;   (require 'elisp-slime-nav)
+;   (defun my-lisp-hook ()
+;     (elisp-slime-nav-mode)
+;     (turn-on-eldoc-mode)
+;     )
+;   (add-hook 'emacs-lisp-mode-hook 'my-lisp-hook)
+; )
+; (evil-define-key 'normal emacs-lisp-mode-map (kbd "K")
+;   'elisp-slime-nav-describe-elisp-thing-at-point)
+
+; (require 'dired-x)
+; (evil-define-key 'normal dired-mode-map "h" 'dired-up-directory)
+; (evil-define-key 'normal dired-mode-map "l" 'dired-find-alternate-file)
+; (evil-define-key 'normal dired-mode-map "o" 'dired-sort-toggle-or-edit)
+; (evil-define-key 'normal dired-mode-map "v" 'dired-toggle-marks)
+; (evil-define-key 'normal dired-mode-map "m" 'dired-mark)
+; (evil-define-key 'normal dired-mode-map "u" 'dired-unmark)
+; (evil-define-key 'normal dired-mode-map "U" 'dired-unmark-all-marks)
+; (evil-define-key 'normal dired-mode-map "c" 'dired-create-directory)
+; (evil-define-key 'normal dired-mode-map "n" 'evil-search-next)
+; (evil-define-key 'normal dired-mode-map "N" 'evil-search-previous)
+; (evil-define-key 'normal dired-mode-map "q" 'kill-this-buffer)
