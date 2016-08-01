022276a3d236a2e913642972d706c55b41b32852
Tibor Simon <tibor@tiborsimon.io>
2016-08-01 07:42:55 +0200

    Org mode started to configure.

--- emacs/config/my-evil.el
+++ emacs/config/my-evil.el
@@ -153,17 +153,6 @@
     (define-key evil-visual-state-map "P" 'evil-destroy-paste-before)
     (define-key evil-visual-state-map "p" 'evil-destroy-paste-after)
 
-    (defun copy-from-osx ()
-    (shell-command-to-string "pbpaste"))
-
-    (defun paste-to-osx (text &optional push)
-    (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-    (process-send-string proc text)
-    (process-send-eof proc))))
-
-    (setq interprogram-cut-function 'paste-to-osx)
-    (setq interprogram-paste-function 'copy-from-osx)
 
     (use-package key-chord
       :ensure key-chord
