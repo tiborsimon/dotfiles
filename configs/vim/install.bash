d20d186ca6b4f3c54537dda6e4a9a91f6aa3a27a
Tibor Simon <tibor@tiborsimon.io>
2018-12-27 00:20:35 +0100

    [vim] Autogroups added, minor config refactor.

--- configs/vim/install.bash
+++ configs/vim/install.bash
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# Install VimPlug to the unlinked local directory structure
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Install ctags for the system
+sudo pacman -S ctags
+
+# Install fzy as the fuzzy search driver
 sudo pacman -S ctags
