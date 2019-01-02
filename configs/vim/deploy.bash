caf31145b6df2bcb28773a77fccd1199d958fd7b
Tibor Simon <tibor@tiborsimon.io>
2019-01-02 10:20:35 +0100

    [vim] SpeedDating added with my ledger extension.

--- configs/vim/deploy.bash
+++ configs/vim/deploy.bash
@@ -6,3 +6,9 @@ source ../../utils/lib/libdeploy.bash
 link_package vim \
              ./config/vimrc ${HOME}/.vimrc \
              ./config/vim ${HOME}/.vim
+
+VIM_SPEEDDATING_CUSTOM_PATH="${HOME}/.vim/plugged/vim-speeddating/after/plugin"
+mkdir -p ${VIM_SPEEDDATING_CUSTOM_PATH}
+
+link_package vim \
+             ./config/speeddating.vim ${VIM_SPEEDDATING_CUSTOM_PATH}/speeddating.vim
