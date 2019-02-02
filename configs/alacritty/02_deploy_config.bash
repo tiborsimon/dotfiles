d0783c99b68d9ebf07ab2aa9547bee4ef263c530
Tibor Simon <tibor@tiborsimon.io>
2019-02-03 00:22:01 +0100

    [alacritty] Config started.

---
+++ configs/alacritty/02_deploy_config.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd $(dirname $(readlink -f $0))
+
+source ../../utils/libdeploy.bash
+
+link_package alacritty \
+             ./config/alacritty.yml ${HOME}/.config/alacritty/alacritty.yml
