3c360b5eea02320fec07398dd4864b81f66307cc
Tibor Simon <tibor@tiborsimon.io>
2019-02-03 15:44:02 +0100

    [fonts] Nerd fonts added, others normalized.

--- configs/fonts/01_install_dependencies.bash
+++ configs/fonts/01_install_dependencies.bash
@@ -3,4 +3,5 @@ cd $(dirname $(readlink -f $0))
 
 source ../../utils/libdeploy.bash
 
-install_packages ttf-dejavu ttf-liberation noto-fonts
+install_packages ttf-dejavu ttf-liberation
+install_aur_packages nerd-fonts-source-code-pro
