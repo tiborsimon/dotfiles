b85435b2f5ea1debf9df5d17617f8201c45f4b69
Tibor Simon <tibor@tiborsimon.io>
2019-02-03 15:44:43 +0100

    [fish] Nerd and powerline fonts turned on.

--- configs/fish/config/fish/config.fish
+++ configs/fish/config/fish/config.fish
@@ -89,8 +89,8 @@ set -g theme_title_display_user         no
 set -g theme_title_use_abbreviated_path yes
 set -g theme_date_format                "+%F %a %H:%M"
 set -g theme_avoid_ambiguous_glyphs     yes
-set -g theme_powerline_fonts            no
-set -g theme_nerd_fonts                 no
+set -g theme_powerline_fonts            yes
+set -g theme_nerd_fonts                 yes
 set -g theme_show_exit_status           yes
 set -g default_user                     tibor
 set -g theme_color_scheme               dark
