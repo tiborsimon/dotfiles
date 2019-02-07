130c86c5e435c86244f41cc919630800413f5db3
Tibor Simon <tibor@tiborsimon.io>
2019-02-07 23:28:03 +0100

    [spacemacs] Spacemacs added again.

---
+++ configs/spacemacs/02_install_spacemacs.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+cd $(dirname $(readlink -f $0))
+
+source ../../utils/libdeploy.bash
+
+function install_spacemacs {
+  if [ ! -d ${HOME}/.emacs.d ]; then
+    git clone https://github.com/syl20bnr/spacemacs ${HOME}/.emacs.d
+  fi
+}
+
+run install_spacemacs
