f6075d8fa2563f8596fe845efcbb46f08dad9432
Tibor Simon <tibor@tiborsimon.io>
2019-02-15 09:14:02 +0100

    [lemonbar] Fonts fine tuned.

--- configs/lemonbar/scripts/config.bash
+++ configs/lemonbar/scripts/config.bash
@@ -11,14 +11,14 @@ DIMENSIONS="${SCREEN_WIDTH}x${BAR_HEIGHT}"
 
 FONT_1_SPEC="SauceCodePro Nerd Font Mono:size=9"
 FONT_2_SPEC="SauceCodePro Nerd Font Mono:size=10"
-FONT_3_SPEC="SauceCodePro Nerd Font Mono:size=11"
-FONT_4_SPEC="SauceCodePro Nerd Font Mono:size=12"
+FONT_3_SPEC="SauceCodePro Nerd Font Mono:size=12"
+FONT_4_SPEC="SauceCodePro Nerd Font Mono:size=16"
 FONT_5_SPEC="SauceCodePro Nerd Font Mono:size=15"
 
 FONT_1_OFFSET="-5"
 FONT_2_OFFSET="-4"
-FONT_3_OFFSET="-3"
-FONT_4_OFFSET="-2"
+FONT_3_OFFSET="-2"
+FONT_4_OFFSET="1"
 FONT_5_OFFSET="0"
 
 FONT_1="%{T1}"
