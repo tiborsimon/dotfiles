f60b6347be76fa6c73f7884f5fbcbbd1eb733357
Tibor Simon <tibor@tiborsimon.io>
2019-02-15 09:14:48 +0100

    [lemonbar] Battery module updated. Every case handled.

--- configs/lemonbar/scripts/modules/battery.bash
+++ configs/lemonbar/scripts/modules/battery.bash
@@ -2,11 +2,11 @@
 
 source $1
 
-ICONS="${FONT_1}\uf579${FONT_1}.${FONT_5}\uf585${FONT_1}|${FONT_1}\uf57a${FONT_1}.${FONT_5}\uf585${FONT_1}|${FONT_1}\uf57b${FONT_1}.${FONT_5}\uf586${FONT_1}|${FONT_1}\uf57c${FONT_1}.${FONT_5}\uf587${FONT_1}|${FONT_1}\uf57d${FONT_1}.${FONT_5}\uf588${FONT_1}|${FONT_1}\uf57e${FONT_1}.${FONT_5}\uf588${FONT_1}|${FONT_1}\uf57f${FONT_1}.${FONT_5}\uf589${FONT_1}|${FONT_1}\uf580${FONT_1}.${FONT_5}\uf589${FONT_1}|${FONT_1}\uf581${FONT_1}.${FONT_5}\uf58a${FONT_1}|${FONT_1}\uf578${FONT_1}.${FONT_5}\uf584${FONT_1}"
-
-ICON_EMPTY="${FONT_1}\uf58d${FONT_1}"
-ICON_UNKNOWN="${FONT_1}\uf590${FONT_1}"
-ICON_FULLY_CHARGED="${FONT_1}\uf58e${FONT_1}"
+ICONS="..ﴆﴇﴈﴉﴊﴋﴌﴍﴎﴅ"
+ICON_EMPTY=""
+ICON_UNKNOWN=""
+ICON_FULLY_CHARGED=""
+ICON_AC="ﮣ"
 
 function render_battery {
   index=$1
@@ -20,25 +20,42 @@ function render_battery {
     'Unknown')
       if (( ${capacity} <= 5 )); then
         echo ${ICON_EMPTY}
-      else
-        echo ${ICON_UNKNOWN}
+        return 0
       fi
-      return 0
+      icon_set=$(echo ${ICONS} | cut -d'.' -f 1)
+      font="${FONT_1}"
       ;;
     'Discharging')
-      type=1;;
+      icon_set=$(echo ${ICONS} | cut -d'.' -f 3)
+      font="${FONT_4}"
+      ;;
     'Charging')
-      type=2;;
+      icon_set=$(echo ${ICONS} | cut -d'.' -f 2)
+      font="${FONT_5}"
+      ;;
   esac
 
-  item=$(echo "${capacity} / 10" | bc)
-  if (( ${item} == 0 )); then
-    item=1
+  item=$(python -c "import math;c=math.floor(((${capacity}+4)/10)-1);print(c)")
+
+  if [[ ${item} == -1 ]]; then
+    echo "${FONT_1}${ICON_EMPTY}${FONT_1}"
+  else
+    echo "${font}${icon_set:${item}:1}${FONT_1}"
   fi
-  echo $(echo ${ICONS} | cut -d'|' -f ${item} | cut -d'.' -f ${type} )
 }
 
+function render_ac {
+  ac_present=$(cat /sys/class/power_supply/AC/online)
+
+  if [[ ${ac_present} -eq 1 ]]; then
+    echo "${FONT_1}${ICON_AC} "
+  else
+    echo ""
+  fi
+}
+
+AC=$(render_ac)
 BATTERY_1=$(render_battery 1)
 BATTERY_2=$(render_battery 0)
 
-echo -en "%{A:battery:}${BATTERY_1} ${BATTERY_2}%{A}${FONT_1}"
+echo -en "%{A:battery:}${AC}${BATTERY_1} ${BATTERY_2}%{A}${FONT_1}"
