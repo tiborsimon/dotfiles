ee3dcc4e28dc4b0a89cc45221488b644fc0dfb6a
Tibor Simon <tibor@tiborsimon.io>
2019-02-23 15:01:19 +0100

    [docker] Initial group handling improved.

--- configs/docker/02_config_user.bash
+++ configs/docker/02_config_user.bash
@@ -3,5 +3,8 @@ cd $(dirname $(readlink -f $0))
 
 source ../../utils/libdeploy.bash
 
-execute_with_privilege sudo usermod -a -G docker $USER
-write_to_messages "  docker - You should log out then log back in to be able to use docker."
+if groups | grep -qv docker
+then
+  execute_with_privilege sudo usermod -a -G docker $USER
+  write_to_messages " docker - You should log out then log back in to be able to use docker."
+fi
