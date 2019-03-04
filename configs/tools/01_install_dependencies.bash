071f39180f95c676d23e2299809945002f75ca3a
Tibor Simon <tibor@tiborsimon.io>
2019-03-04 21:16:37 +0100

    [tools] Skype and maim added.

--- configs/tools/01_install_dependencies.bash
+++ configs/tools/01_install_dependencies.bash
@@ -3,4 +3,13 @@ cd $(dirname $(readlink -f $0))
 
 source ../../utils/libdeploy.bash
 
+# General tools
 install_packages bc tree htop
+
+# Skype - keychain needed to be able to stay logged in.
+install_aur_packages skypeforlinux-stable-bin
+install_packages gnome-keychain
+
+# Screenshot
+install_packages maim
+
