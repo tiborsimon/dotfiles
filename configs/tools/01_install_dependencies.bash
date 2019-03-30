68daedaff500e84499cf0808814d11a130b4d2d5
Tibor Simon <tibor@tiborsimon.io>
2019-03-30 17:48:31 +0100

    [tools] Tools list updated.

--- configs/tools/01_install_dependencies.bash
+++ configs/tools/01_install_dependencies.bash
@@ -4,7 +4,7 @@ cd $(dirname $(readlink -f $0))
 source ../../utils/libdeploy.bash
 
 # General tools
-install_packages bc tree htop
+install_packages bc tree htop wget
 
 # Skype - keychain needed to be able to stay logged in.
 install_aur_packages skypeforlinux-stable-bin
@@ -13,3 +13,14 @@ install_packages gnome-keychain
 # Screenshot
 install_packages maim
 
+# DNS related tools
+install_packages dnsutils
+
+# OpenVPN
+install_packages openvpn systemd-resolveconf
+
+# Networking packages
+install_packages traceroute
+
+# Removable media handler
+install_packages udisks2
