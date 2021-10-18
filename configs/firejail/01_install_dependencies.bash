94e57c77c1fad091a65771fb26b29be95e01b87d
Tibor Simon <tibor@tiborsimon.io>
2021-10-18 17:38:58 +0200

    Before directory structure change

--- configs/firejail/01_install_dependencies.bash
+++ configs/firejail/01_install_dependencies.bash
@@ -6,4 +6,5 @@ source ../../utils/libdeploy.bash
 install_packages firejail xdg-dbus-proxy
 
 write_to_messages " firejail - You should manually run the ${BOLD}firecfg${RESET} command to add the shims!"
-write_to_messages " firejail - You should manually edit the ${BOLD}browser-allow-drm${RESET} config in the ${BOLD}/etc/firejail/firejail.config${RESET} file!"
+write_to_messages " firejail - You should manually edit the ${BOLD}browser-allow-drm${RESET} config to 'yes' in the ${BOLD}/etc/firejail/firejail.config${RESET} file!"
+write_to_messages " firejail - You should manually edit the ${BOLD}browser-disable-u2f${RESET} config to 'no' in the ${BOLD}/etc/firejail/firejail.config${RESET} file!"
--- configs/sway/01_install_dependencies.bash
+++ configs/sway/01_install_dependencies.bash
@@ -4,7 +4,7 @@ cd "$(dirname "$(readlink -f "$0")")" || exit
 source ../../utils/libdeploy.bash
 
 # Basic sway packages.
-install_packages sway swayidle waybar
+install_packages sway swayidle waybar wayland-protocols
 install_aur_packages swaylock-effects-git
 
 # For screenshot management.
@@ -15,6 +15,12 @@ install_packages jq
 
 # Brightness settings.
 install_packages brightnessctl
+
+# Notification manager
+install_packages mako libnotify
+
+# Audio control
+install_packages pulseaudio pulseaudio-alsa
 
 # Xorg compatibility until all used program is ported to pure wayland..
 install_packages xorg-xwayland
