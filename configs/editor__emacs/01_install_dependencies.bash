1e708c059cef79a6aeebfc54618bcbef61d7a922
Tibor Simon <tibor@tiborsimon.io>
2021-11-08 20:15:51 +0100

    Xwayland disabled, pdf viewer dependencies added to emacs

--- configs/editor__emacs/01_install_dependencies.bash
+++ configs/editor__emacs/01_install_dependencies.bash
@@ -3,6 +3,11 @@ cd "$(dirname "$(readlink -f "$0")")" || exit
 
 source ../../utils/libdeploy.bash
 
+# Special font.
 install_packages bdf-unifont
+
+# To be able to render pdf documents in emacs.
+install_packages ghostscript
+
 # To be able to run natively on wayland.
 install_aur_packages emacs-gcc-wayland-devel-bin
